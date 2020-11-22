2c7c594a0fcb7a4a3a021d9b26e4fe9a61c756b6
monaqa <mogassy@yahoo.co.jp>
2020-11-22 21:07:20 +0900

    add integer increment

--- lua/dial.lua
+++ lua/dial.lua
@@ -2,15 +2,46 @@ local function dbg(...)
     print(vim.inspect(...))
 end
 
+-- cursor 上にあるか、もしくは cursor の後にある数字を検索する。
+local function search_decimal(line, cursor)
+    local idx_start = 1
+    local s, e
+    while idx_start < #line do
+        s, e = string.find(line, "-?%d+", idx_start)
+        if s then
+            -- 検索結果が見つかれば
+            if (cursor <= e) then  -- cursor が終了文字より後ろにあればそれが答え
+                return s, e, string.sub(line, s, e)
+            else
+                idx_start = e + 1
+            end
+        else
+            -- 検索結果がなければ nil を return
+            break
+        end
+    end
+    return nil
+end
+
+-- インクリメントする。
 local function increment(addend)
-    -- インクリメントする。
-    local bufnum, lnum, col, off, curswant = vim.call('getcurpos')
+
+    -- 現在のカーソル位置、カーソルのある行の取得
+    local curpos = vim.call('getcurpos')
+    local col_cursor = curpos[2]
     local line = vim.fn.getline('.')
 
-    local newline = line .. 'a'
-    local newcol = #newline
+    -- 数字の検索、加算後のテキストの作成
+    local s, e, text = search_decimal(line, col_cursor)
+    local num = tonumber(text)
+    local newnum = num + addend
+    local newtext = tostring(newnum)
+    local newline = string.sub(line, 1, s - 1) .. newtext .. string.sub(line, e + 1)
+    local newcol = s - 1 + #newtext  -- newtext の末尾に持ってくる
+
+    -- 行編集、カーソル位置のアップデート
     vim.fn.setline('.', newline)
-    vim.fn.setpos('.', {bufnum, lnum, col, off})
+    vim.fn.setpos('.', {curpos[1], curpos[2], newcol, curpos[4], curpos[5]})
 end
 
 return {
--- plugin/dial.vim
+++ plugin/dial.vim
@@ -4,8 +4,8 @@ let s:save_cpo = &cpo " save user coptions
 set cpo&vim " reset them to defaults
 
 " command to run our plugin
-nmap <expr> <Plug>(dial-increment) '<Cmd>lua require"dial".increment(' .. v:count1 .. ')<CR>'
-nmap <expr> <Plug>(dial-decrement) '<Cmd>lua require"dial".increment(' .. -v:count1 .. ')<CR>'
+nnoremap <expr> <Plug>(dial-increment) '<Cmd>lua require"dial".increment(' .. v:count1 .. ')<CR>'
+nnoremap <expr> <Plug>(dial-decrement) '<Cmd>lua require"dial".increment(' .. -v:count1 .. ')<CR>'
 
 let &cpo = s:save_cpo " and restore after
 unlet s:save_cpo
