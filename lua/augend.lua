035220b22b4de027bf00e5c79d54d43a236419fb
monaqa <mogassy@yahoo.co.jp>
2021-01-03 11:09:35 +0900

    def pickup_augend function

--- lua/augend.lua
+++ lua/augend.lua
@@ -34,52 +34,6 @@ function find_pattern(ptn)
     return f
 end
 
-function get_range(span)
-    return span.from, span.to
-end
-
-function status(span, cursor)
-    -- cursor 入力が与えられたとき、
-    -- 自らのスパンが cursor に対してどのような並びになっているか出力する。
-    -- span が cursor を含んでいるときは0、
-    -- span が cursor より前方にあるときは 1、
-    -- span が cursor より後方にあるときは 2 を出力する。
-    -- この数字は採用する際の優先順位に相当する。
-    local s, e = get_range(span)
-    if cursor < s then
-        return 1
-    elseif cursor > e then
-        return 2
-    else
-        return 0
-    end
-end
-
-function comp_with_corsor(cursor)
-    -- カーソル位置が与えられた下での範囲の優先順位を定める comp 関数を返す。
-    -- ルールは以下。
-    -- c が s と e の間にあるものが最優先。
-    -- c が s と e の手前にあるものが次。
-    -- c が s と e の後ろにあるものは最も低め。
-    -- その上で、 s が手前にあるものを優先、
-    -- e が後ろにあるものを優先する。
-    local function comp(span1, span2)
-        if span1:status(cursor) ~= span2:status(cursor) then
-            return span1:status(cursor) < span2:status(cursor)
-        else
-            local s1, e1 = get_range(span1)
-            local s2, e2 = get_range(span2)
-            if s1 ~= s2 then
-                return s1 < s2
-            else
-                return e1 > e2
-            end
-        end
-    end
-    return comp
-end
-
 return {
     find_pattern = find_pattern,
-    comp_with_corsor = comp_with_corsor,
 }
--- lua/dial.lua
+++ lua/dial.lua
@@ -13,6 +13,58 @@ M.available_augends = {
     number.decimal_integer,
 }
 
+local function status(span, cursor)
+    -- cursor 入力が与えられたとき、
+    -- 自らのスパンが cursor に対してどのような並びになっているか出力する。
+    -- span が cursor を含んでいるときは0、
+    -- span が cursor より前方にあるときは 1、
+    -- span が cursor より後方にあるときは 2 を出力する。
+    -- この数字は採用する際の優先順位に相当する。
+    local s, e = span.from, span.to
+    if cursor < s then
+        return 1
+    elseif cursor > e then
+        return 2
+    else
+        return 0
+    end
+end
+
+function M.pickup_augend(lst, cursor)
+    -- 現在の cursor 位置をもとに、
+    -- span: {augend: augend, from: int, to:int} を要素に持つ配列 lst から
+    -- 適切な augend を一つ取り出す。
+    local function comp(span1, span2)
+        -- span1 の優先順位が span2 よりも高いかどうか。
+        -- まずは status（カーソルとspanの位置関係）に従って優先順位を決定する。
+        -- 両者の status が等しいときは、開始位置がより手前にあるものを選択する。
+        -- 開始位置も等しいときは、終了位置がより奥にあるものを選択する。
+        if status(span1, cursor) ~= status(span2, cursor) then
+            return status(span1, cursor) < status(span2, cursor)
+        else
+            local s1, e1 = span1.from, span1.to
+            local s2, e2 = span2.from, span2.to
+            if s1 ~= s2 then
+                return s1 < s2
+            else
+                return e1 > e2
+            end
+        end
+    end
+
+    local span = lst[1]
+    if span == nil then
+        return nil
+    end
+
+    for _, s in pairs(lst) do
+        if comp(s, span) then
+            span = s
+        end
+    end
+    return span
+end
+
 -- インクリメントする。
 function M.increment(addend)
 
@@ -25,18 +77,27 @@ function M.increment(addend)
     end
 
     -- 数字の検索、加算後のテキストの作成
-    local idxlst = util.filter_map_zip(function(aug) return aug.find(cursor, line) end, M.available_augends)
+    local auglst = util.filter_map(
+        function(aug)
+            span = aug.find(cursor, line)
+            if span == nil then
+                return nil
+            end
+            return {augend = aug, from = span.from, to = span.to}
+        end,
+        M.available_augends
+    )
     -- TODO: 最優先の span を取ってこれるようにする
     -- TODO: sort っていうか min でよくね？
-    --     table.sort(idxlst, comp_with_corsor(cursor))
+    --     table.sort(auglst, comp_with_corsor(cursor))
     -- ひとまず今は一番手前のやつをとる
-    local elem = idxlst[1]
+    local elem = M.pickup_augend(auglst, cursor)
     if elem == nil then
         return
     else
-        local aug = elem[1]
-        local span = elem[2]
-        local s, e = get_range(span)
+        local aug = elem.augend
+        local s = elem.from
+        local e = elem.to
         local rel_cursor = cursor - s + 1
         local text = string.sub(line, s, e)
         local newcol, text = aug.add(rel_cursor, text, addend)
