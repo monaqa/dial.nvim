11376d2cd94dbee81ff134eb4b73782ee4961981
monaqa <mogassy@yahoo.co.jp>
2021-01-12 23:43:41 +0900

    add date["%-m/%-d"]

--- lua/augends/date.lua
+++ lua/augends/date.lua
@@ -41,7 +41,7 @@ M["%Y/%m/%d"] = {
 
 M["%m/%d"] = {
     name = "date['%m/%d']",
-    desc = "standard date %m/%d",
+    desc = "standard date %m/%d (01/01, 02/28, 12/25, etc.)",
 
     find = common.find_pattern("%d%d/%d%d"),
     add = function(cursor, text, addend)
@@ -58,6 +58,67 @@ M["%m/%d"] = {
         local thisyear = os.date("*t").year
         local date = os.time{year=thisyear, month=month, day=day}
         text = os.date("%m/%d", date)
+        return cursor, text
+    end
+}
+
+M["%-m/%-d"] = {
+    name = "date['%-m/%-d']",
+    desc = "standard date %-m/%-d (1/1, 2/28, 12/25, etc.)",
+
+    find = function(cursor, line)
+        local range = common.find_pattern("%d%d?/%d%d?")(cursor, line)
+        if range == nil then
+            return nil
+        end
+        local text = line:sub(range.from, range.to)
+        local strips = vim.split(text, "/")
+        local month, day = tonumber(strips[1]), tonumber(strips[2])
+
+        -- check whether a valid date or not
+        if month == 0 or month > 12 or day == 0 then
+            return nil
+        end
+        local month_day_29 = (month == 2) and day <= 29
+        local month_day_30 = vim.tbl_contains({4, 6, 9, 11}, month) and day <= 30
+        local month_day_31 = vim.tbl_contains({1, 3, 5, 7, 8, 10, 12}, month) and day <= 31
+
+        if month_day_29 or month_day_30 or month_day_31 then
+            return range
+        end
+
+        return nil
+    end,
+    add = function(cursor, text, addend)
+        local _, _, month_str, day_str = text:find("(%d%d?)/(%d%d?)")
+        local month_s, month_e = 1, #month_str
+        local day_s, day_e = #month_str + 2, #text
+        local month, day = tonumber(month_str), tonumber(day_str)
+
+        if cursor == nil then cursor = day_e end  -- default: day
+        local objective
+        if cursor >= month_s and cursor <= month_e then
+            month = month + addend
+            objective = "month"
+        else
+            day = day + addend
+            objective = "day"
+        end
+        local thisyear = os.date("*t").year
+        local date = os.time{year=thisyear, month=month, day=day}
+        text = os.date("%-m/%-d", date)
+
+        -- determine cursor position
+        if objective == "month" then
+            if date.month >= 10 then
+                cursor = 2
+            else
+                cursor = 1
+            end
+        else
+            cursor = #text
+        end
+
         return cursor, text
     end
 }
