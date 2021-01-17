046237d17089ebf21210507f634c265a2b932998
monaqa <mogassy@yahoo.co.jp>
2021-01-17 19:49:35 +0900

    update default augends

--- lua/default.lua
+++ lua/default.lua
@@ -9,11 +9,27 @@ M.searchlist = {}
 M.searchlist.normal = {
     "number#decimal",
     "number#hex",
+    "number#binary",
+    "date#[%Y/%m/%d]",
+    "date#[%m/%d]",
+    "date#[%Y-%m-%d]",
+    "date#[%H:%M]",
+    "date#[%jA]",
+    "color#hex",
 }
 
 M.searchlist.visual = {
     "number#decimal",
     "number#hex",
+    "number#binary",
+    "date#[%Y/%m/%d]",
+    "date#[%m/%d]",
+    "date#[%Y-%m-%d]",
+    "date#[%H:%M]",
+    "date#[%jA]",
+    "color#hex",
+    "char#alph#small",
+    "char#alph#capital",
 }
 
 return M
