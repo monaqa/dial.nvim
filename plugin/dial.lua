21f1b0df166657c239f97e282387e1567218b28c
monaqa <cmonaqa@gmail.com>
2025-08-30 16:16:39 +0900

    fix!: remove g-prefixed mapping and rename Plug mapping
    Fixes #113. This mapping matters in SELECT mode.

--- plugin/dial.lua
+++ plugin/dial.lua
@@ -7,12 +7,12 @@ vim.cmd [[set cpo&vim]]
 
 vim.keymap.set("n", "<Plug>(dial-increment)", require("dial.map").inc_normal())
 vim.keymap.set("n", "<Plug>(dial-decrement)", require("dial.map").dec_normal())
-vim.keymap.set("n", "g<Plug>(dial-increment)", require("dial.map").inc_gnormal())
-vim.keymap.set("n", "g<Plug>(dial-decrement)", require("dial.map").dec_gnormal())
+vim.keymap.set("n", "<Plug>(dial-g-increment)", require("dial.map").inc_gnormal())
+vim.keymap.set("n", "<Plug>(dial-g-decrement)", require("dial.map").dec_gnormal())
 vim.keymap.set("v", "<Plug>(dial-increment)", require("dial.map").inc_visual() .. "gv")
 vim.keymap.set("v", "<Plug>(dial-decrement)", require("dial.map").dec_visual() .. "gv")
-vim.keymap.set("v", "g<Plug>(dial-increment)", require("dial.map").inc_gvisual() .. "gv")
-vim.keymap.set("v", "g<Plug>(dial-decrement)", require("dial.map").dec_gvisual() .. "gv")
+vim.keymap.set("v", "<Plug>(dial-g-increment)", require("dial.map").inc_gvisual() .. "gv")
+vim.keymap.set("v", "<Plug>(dial-g-decrement)", require("dial.map").dec_gvisual() .. "gv")
 
 vim.o.cpoptions = cpo
 vim.g.loaded_dial = 1
