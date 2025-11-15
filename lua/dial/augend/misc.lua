1eb433faa1594ed56dd959c48988948984b08a4b
NeonCarbide <deadstream37@gmail.com>
2025-11-15 02:14:26 -0500

    feat: Add english short ordinals as a new misc.alias

--- lua/dial/augend/misc.lua
+++ lua/dial/augend/misc.lua
@@ -37,4 +37,40 @@ M.alias.markdown_header = user.new {
     end,
 }
 
+M.alias.ordinals = user.new {
+    ---@param line string
+    ---@param cursor? integer
+    ---@return textrange?
+    find = function(line, cursor)
+        local mark_start, mark_end = line:find "-?%d+%a%a"
+        local _, check_end = line:find "-?%d+%a+"
+
+        if mark_start == nil or check_end ~= mark_end then
+            return nil
+        end
+
+        return { from = mark_start, to = mark_end }
+    end,
+    ---@param text string
+    ---@param addend integer
+    ---@param cursor? integer
+    ---@return { text?: string, cursor?: integer }
+    add = function(text, addend, cursor)
+        local special_suffix = { "st", "nd", "rd" }
+
+        for ordinal in text:gmatch "-?%d+" do
+            local cardinal = ordinal + addend
+            local remainder = math.abs(cardinal) % 100
+
+            local suffix = not vim.tbl_contains({ 11, 12, 13 }, remainder) and special_suffix[remainder % 10] or "th"
+
+            text = cardinal .. suffix
+        end
+
+        cursor = 1
+
+        return { text = text, cursor = cursor }
+    end,
+}
+
 return M
